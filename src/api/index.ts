4531a00a35b90a8363b73dbde55c89aba405a0f3
EarthlyZ9 <earthlyz9.dev@gmail.com>
2025-12-09 13:19:43 +0900

    feat: call backend api via api module

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,6 @@
-import {
-  isPermissionGranted,
-  requestPermission,
-  Schedule,
-  sendNotification,
-} from '@tauri-apps/plugin-notification';
+import { Task } from '@/types';
+import { invoke } from '@tauri-apps/api/core';
+import { isPermissionGranted, requestPermission } from '@tauri-apps/plugin-notification';
 
 export const checkNotificationPermission = async () => {
   let permissionGranted = await isPermissionGranted();
@@ -13,11 +10,29 @@ export const checkNotificationPermission = async () => {
   }
 };
 
-type NotificationExtra = Record<string, any>;
+export const getTasks = async () => {
+  try {
+    const result = await invoke<Task[]>('get_tasks');
+    console.log(result); // TODO: remove this log
+    return result;
+  } catch (error) {
+    console.error('Failed to fetch tasks:', error);
+  }
+};
 
-export const scheduleNotification = (id: number, body: string, scheduleDt: Date) => {
-  const title = 'Nagging...';
-  const extra: NotificationExtra = { taskId: id };
-  sendNotification({ title, body, schedule: Schedule.at(scheduleDt), extra });
-  console.log('Notification scheduled:', { title, body, scheduleDt, extra });
+export const addTask = async (description: string, scheduledAt: Date) => {
+  const newTask = await invoke<Task>('add_task', {
+    description: description,
+    scheduledAt: scheduledAt.toISOString(),
+  });
+
+  return newTask;
+};
+
+export const resolveTask = async (taskId: number) => {
+  await invoke('resolve_task', { id: taskId });
+};
+
+export const removeTask = async (taskId: number) => {
+  await invoke('remove_task', { id: taskId });
 };
